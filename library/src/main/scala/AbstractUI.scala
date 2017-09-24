3082fce81a17049d3cfc18420e95185488f6fe65
Laurent DELERIS <laurent.deleris@hotmail.fr>
2017-09-24 21:54:59 +0200

    refactoring with synchronized state

--- library/src/main/scala/AbstractUI.scala
+++ library/src/main/scala/AbstractUI.scala
@@ -1,30 +1,27 @@
 package com.deleris.tetrix
-import java.{util => ju}
 
 class AbstractUI {
-  private[this] val initialState = Stage.newState(Nil,
-    (10, 23), Stage.randomStream(new scala.util.Random))
+  import Stage._
+  import java.{util => ju}
+
+  private[this] val initialState = newState(Nil,
+    (10, 23), randomStream(new scala.util.Random))
   private[this] var state = initialState
   private[this] val timer = new ju.Timer 
   timer.scheduleAtFixedRate(new ju.TimerTask {
-      def run { state = Stage.tick(state) }
+      def run { state = tick(state) }
   }, 0, 1000)
 
-  def left() {
-    state = Stage.moveLeft(state)
-    
-  }
-  def right() {
-    state = Stage.moveRight(state)
-  }
-  def up() {
-    state = Stage.rotateCW(state)
-  }
-  def down() {
-    state = Stage.tick(state)
-  }
-  def space() {
-    state = Stage.drop(state)
-  }
+  def left() = updateState {moveLeft}
+  def right() = updateState {moveRight}
+  def up() = updateState {rotateCW}
+  def down() = updateState {tick}
+  def space() = updateState {drop}
   def view: GameView = state.view
+
+  private[this] def updateState(trans: GameState => GameState) {
+    synchronized {
+      state = trans(state)
+    }
+  }
 }
